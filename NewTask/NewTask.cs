3dd34cd7f5d0ec009f35eabc5d4d0a6ca3c1e5a5
Robert Macias <Robert.Macias@trialcard.com>
2022-12-27 12:22:30 -0500

    feat: Adding a worker (subscriber who works and completes tasks) and a new task maker (publisher who sends work to be done).

--- NewTask/NewTask.cs
+++ NewTask/NewTask.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+public class NewTask {
+    public static void Main (string[] args) {
+        var factory = new ConnectionFactory() {
+            HostName = "localhost", 
+            Port = 5672
+        };
+
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel()) {
+            channel.QueueDeclare(
+                queue: "worker-queue",
+                durable: false, 
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+            );
+
+            string message = GetMessage(args);
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(
+                exchange: "",
+                routingKey: "worker-queue",
+                basicProperties: null,
+                body: body
+            );
+
+            Console.WriteLine($"[x] Sent {message}");
+        }
+
+        Console.WriteLine("Press [enter] to exit.");
+        Console.ReadLine();
+    }
+
+    public static string GetMessage(string[] args) {
+        return ((args.Length > 0)? string.Join(" ", args) : "Hello World!");
+    }
+}
--- Worker/Worker.cs
+++ Worker/Worker.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+public class Worker {
+    public static void Main() {
+        var factory = new ConnectionFactory() {
+            HostName = "localhost",
+            Port = 5672
+        };
+
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel()) {
+            channel.QueueDeclare( 
+                queue: "worker-queue",
+                durable: false, 
+                exclusive: false,
+                autoDelete: false, 
+                arguments: null
+            );
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) => {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($" [x] Received {message}");
+
+                // Fake Task:
+                int dots = message.Split('.').Length - 1;
+                Thread.Sleep(dots * 1000);
+
+                Console.WriteLine($" [x] Task Done");
+            };
+
+            channel.BasicConsume(
+                queue: "worker-queue",  // routingKey
+                autoAck: true,
+                consumer: consumer
+            );
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
